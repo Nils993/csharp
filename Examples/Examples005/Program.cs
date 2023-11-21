0438cfd0a3e271efa87370c94494a3e55ff3f837
Pavel.S <nils010493@yandex.ru>
2023-11-21 13:00:15 +0500

    ex005

---
+++ Examples/Examples005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("введите имя пользователя");
+string username = Console.ReadLine();
+if (username.ToLower ()== "Pavel")
+{
+    Console.WriteLine("Ура это Павел");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
